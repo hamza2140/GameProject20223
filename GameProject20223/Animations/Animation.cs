a9bda7d9425961937b04d78408fc4383b1c47e18
hamza <hamza2140@outlook.com>
2023-12-25 16:40:02 +0100

    Variables Hero

--- GameProject20223/Animations/Animation.cs
+++ GameProject20223/Animations/Animation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,35 @@ namespace GameProject20223.Animations
         {
             frames = new List<AnimationFrame>();
         }
-    }
+        private double secondCounter = 0;
+        public void AddSpriteRow(int width, int height, int row, int numberOfSpritesInRow)
+        {
+            for (int i = 0; i < numberOfSpritesInRow; i++)
+            {
+                frames.Add(new AnimationFrame(new Rectangle(width * i, row * height, width, height)));
+            }
+        }
+        public void Update(GameTime gameTime)
+        {
+            int fps = 15;
+            CurrFrame = frames[counter];
+
+            secondCounter += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (secondCounter >= 1d / fps)
+            {
+                counter++;
+                secondCounter = 0;
+            }
+
+            if (counter >= frames.Count)
+            {
+                counter = 0;
+            }
+            CurrFrame = frames[counter];
+            if (counter >= frames.Count)
+            {
+                counter = 0;
+            }
+        }
 }
--- GameProject20223/Character/Hero.cs
+++ GameProject20223/Character/Hero.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GameProject20223.Animations;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +12,17 @@ namespace GameProject20223.Character
     // Bron jump + gravity: Oyyou. (2012, 8 februari). XNA Tutorial 23 - Jumping and Gravity. YouTube. https://www.youtube.com/watch?v=ZLxIShw-7ac
     class Hero
     {
+        // Variabelen initialiseren
+        bool hasJumped, reachedTop;
+        public Rectangle rectangle;
 
+        private Texture2D heroTexture;
+        private Vector2 position;
+        private Vector2 speed;
+        private int scale, width, height;
+        private SpriteEffects se = SpriteEffects.None;
+
+        Animation runAnimation, attackAnimation, staticAnimation, jumpAnimation, deathAnimation;
+        AnimationManager animationManager;
     }
 }
