690889a8432d86afcb12e3a4a7d13582f301def4
hamza <hamza2140@outlook.com>
2023-12-27 18:34:13 +0100

    IDrawble Interface maken en SOLID proberen toepassen

---
+++ GameProject20223/Classes/GameStates/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.GameStates
+{
+    internal class MainMenu
+    {
+
+    }
+}
---
+++ GameProject20223/Classes/Interfaces/IDrawable.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Interfaces
+{
+    internal interface IDrawable
+    {
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
