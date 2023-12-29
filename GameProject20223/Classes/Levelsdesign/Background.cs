424f9bd66c4607917f963c42b0107070862c59c7
hamza <hamza2140@outlook.com>
2023-12-29 20:10:59 +0100

    Alles spritesheets inladen

--- GameProject20223/Classes/Levelsdesign/Background.cs
+++ GameProject20223/Classes/Levelsdesign/Background.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GameProject20223.Classes.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace GameProject20223.Classes.Levelsdesign
 {
-    internal class Background
+    internal class Background : IGameObject
     {
+        private Texture2D bgTexture;
+
+        public Vector2 WindowSize { get; private set; } = new Vector2(1400, 787);
+
+        public Background(Texture2D texture)
+        {
+            // Set the background texture that was given 
+            bgTexture = texture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the background on the given spriteBatch
+            spriteBatch.Draw(bgTexture, new Vector2(0, 0),
+                 Color.White);
+        }
     }
 }
