f6d612add2c2010cb49ddfcf73d68eaaf2992bc4
hamza <hamza2140@outlook.com>
2024-01-02 15:43:08 +0100

    IDieable interface makne

--- GameProject20223/Classes/Character/Hero.cs
+++ GameProject20223/Classes/Character/Hero.cs
@@ -1,5 +1,6 @@
 ﻿using GameProject20223.Animations;
 using GameProject20223.Classes;
+using GameProject20223.Classes.Character;
 using GameProject20223.Interfaces;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,7 +13,7 @@ using System.Threading.Tasks;
 namespace GameProject20223.Character
 {
     // Bron jump + gravity: Oyyou. (2012, 8 februari). XNA Tutorial 23 - Jumping and Gravity. YouTube. https://www.youtube.com/watch?v=ZLxIShw-7ac
-    class Hero
+    class Hero : IDrawable, IUpdateable
     {
         // Variabelen initialiseren
         bool hasJumped, reachedTop;
