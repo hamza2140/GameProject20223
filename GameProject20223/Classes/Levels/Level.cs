156a7ec07cdbf3c0c3d56e3dac842cc380a5fea6
hamza <hamza2140@outlook.com>
2024-01-02 17:40:02 +0100

    Beginnen aan de level klasse, variable initialiseren

--- GameProject20223/Classes/Levels/Level.cs
+++ GameProject20223/Classes/Levels/Level.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GameProject20223.Classes.Character;
+using GameProject20223.Classes.Collectable;
+using GameProject20223.Classes.Levelsdesign;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -7,14 +10,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrayNotify;
+using Background = GameProject20223.Classes.Levelsdesign.Background;
 
 namespace GameProject20223.Classes.Levels
 {
     public abstract class Level
     {
-        
-       
+        public Map map;
+        public List<Star> stars;
+        public int MaxScore
+        {
+            get { return enemies.Count + stars.Count; }
+        }
 
-        
+        internal Background background;
+        internal Texture2D _backgroundTexture, _dragonTexture, _zombieTexture, _trapTexture;
+        internal List<Enemy> enemies;
+        internal ContentManager _content;
+        internal GraphicsDevice _graphicsDevice;
+
+
+
     }
 }
