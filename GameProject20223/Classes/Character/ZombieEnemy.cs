b101d7b374b10c7581a3de5549e4bf343c45534c
hamza <hamza2140@outlook.com>
2024-01-02 18:29:50 +0100

    ZombieEnemy klassen maken en construcor initialiseren

---
+++ GameProject20223/Classes/Character/ZombieEnemy.cs
@@ -0,0 +1,29 @@
+﻿using GameProject20223.Animations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Character
+{
+    class ZombieEnemy : Enemy
+    {
+        Animation runAnimation, attackAnimation, deathAnimation;
+
+        public zombieEnemy(Texture2D texture, int startPos, int endPos, int height) : base(texture, startPos, endPos, height)
+        {
+            // Zombie trager maken
+            base.speed.X = 2;
+            base.width = 64;
+            base.height = 64;
+            base.scale = 2;
+
+            // Animaties ingeven.
+            MakeAnimations();
+
+            // Huidige animatie intialiseren.
+            animationManager.CurrentAnimation = runAnimation;
+        }
+    }
+}
