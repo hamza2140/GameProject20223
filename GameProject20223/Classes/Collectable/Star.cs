c65c66e0bb44b8700cc6d23ed20617f945e754fc
hamza <hamza2140@outlook.com>
2024-01-02 17:15:56 +0100

    Star klasse maken en variables

---
+++ GameProject20223/Classes/Collectable/Star.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Collectable
+{
+    public class Star
+    {
+        Texture2D startTexture;
+        private bool collected;
+
+        private int x { get; set; }
+        private int y { get; set; }
+
+        private int size { get; set; }
+
+        public Rectangle rectangle;
+    }
+}
