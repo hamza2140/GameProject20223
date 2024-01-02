f17c8e465149e1ff29b9069e9103221cc759ae2c
hamza <hamza2140@outlook.com>
2024-01-02 18:08:50 +0100

    DragonEnemy klasse maken en constructor implementeren

---
+++ GameProject20223/Classes/Character/DragonEnemy.cs
@@ -0,0 +1,30 @@
+﻿using GameProject20223.Animations;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Character
+{
+    public class DragonEnemy : Enemy
+    {
+        Animation runAnimation, attackAnimation, deathAnimation;
+
+        public dragonEnemy(Texture2D texture, int startPos, int endPos, int height) : base(texture, startPos, endPos, height)
+        {
+            // Default waardes ingeven
+            base.width = 32;
+            base.height = 32;
+            base.scale = 4;
+
+            // Animaties ingeven.
+            MakeAnimations();
+
+            // Huidige animatie intialiseren.
+            animationManager = new AnimationManager();
+            animationManager.CurrentAnimation = runAnimation;
+        }
+    }
+}
