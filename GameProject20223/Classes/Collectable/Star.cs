1c00280739e0fec58522ec35e049ed0fc54d171d
hamza <hamza2140@outlook.com>
2024-01-02 17:17:46 +0100

    Star klasse updaten met constructor en draw methode

--- GameProject20223/Classes/Collectable/Star.cs
+++ GameProject20223/Classes/Collectable/Star.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GameProject20223.Character;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -19,5 +21,35 @@ namespace GameProject20223.Classes.Collectable
         private int size { get; set; }
 
         public Rectangle rectangle;
+        public Star(ContentManager content, int xPos, int yPos)
+        {
+            x = xPos;
+            y = yPos;
+
+            size = 50;
+
+            startTexture = content.Load<Texture2D>("star");
+            rectangle = new Rectangle(xPos, yPos, size, size);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (!collected)
+            {
+                spriteBatch.Draw(startTexture, rectangle, Color.White);
+            }
+        }
+
+        public void Collected()
+        {
+            // Coin is opgenomen
+            collected = true;
+
+            // Uit scherm halen
+            rectangle.Y = -500;
+
+            // Score verhogen
+            Hero.score += 1;
+        }
     }
 }
