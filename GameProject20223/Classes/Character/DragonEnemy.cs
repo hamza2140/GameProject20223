97536353d74fcf39bf8784e7929010f1b7d583ac
hamza <hamza2140@outlook.com>
2024-01-02 18:12:59 +0100

    MakeAnimations in DragonEnemy klasse aanmaken

--- GameProject20223/Classes/Character/DragonEnemy.cs
+++ GameProject20223/Classes/Character/DragonEnemy.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace GameProject20223.Classes.Character
 {
-    public class DragonEnemy : Enemy
+    class DragonEnemy : Enemy
     {
         Animation runAnimation, attackAnimation, deathAnimation;
 
-        public dragonEnemy(Texture2D texture, int startPos, int endPos, int height) : base(texture, startPos, endPos, height)
+        public DragonEnemy(Texture2D texture, int startPos, int endPos, int height) : base(texture, startPos, endPos, height)
         {
             // Default waardes ingeven
             base.width = 32;
@@ -25,6 +25,18 @@ namespace GameProject20223.Classes.Character
             // Huidige animatie intialiseren.
             animationManager = new AnimationManager();
             animationManager.CurrentAnimation = runAnimation;
+        }
+        internal override void MakeAnimations()
+        {
+            // Set all the animation for the hero
+            deathAnimation = new Animation();
+            deathAnimation.AddSpriteRow(width, height, 0, 11);
+
+            runAnimation = new Animation();
+            runAnimation.AddSpriteRow(width, height, 1, 4);
+
+            attackAnimation = new Animation();
+            attackAnimation.AddSpriteRow(width, height, 2, 7);
         }
     }
 }
