78be4f08339ebdd9dcae76f254ae5bdb9a4d4054
hamza <hamza2140@outlook.com>
2024-01-04 17:27:44 +0100

    Contructor initialiseren in GameOver Klasse

--- GameProject20223/Classes/GameStates/GameOver.cs
+++ GameProject20223/Classes/GameStates/GameOver.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GameProject20223.Classes.Button;
+using GameProject20223.Classes.Levelsdesign;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,20 @@ namespace GameProject20223.Classes.GameStates
 {
     internal class GameOver : GameState
     {
+        private Texture2D _backgroundTexture;
+        private Background background;
 
+        public GameOver(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            // Textures inladen
+            _backgroundTexture = _content.Load<Texture2D>("gameover");
+
+            // Initialiseren
+            background = new Background(_backgroundTexture);
+
+            buttons.Add(new MenuButton(game, graphicsDevice, content, 610, 400));
+            buttons.Add(new RestartButton(game, graphicsDevice, content, 610, 480));
+            buttons.Add(new CloseButton(game, graphicsDevice, content, 610, 560));
+        }
     }
 }
