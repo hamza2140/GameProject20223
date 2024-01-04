a21ad73c3d24de504267ec1de9563adf959706b5
hamza <hamza2140@outlook.com>
2024-01-04 17:21:43 +0100

    RestartButton klasse maken en inheritance implementeren(DoBtnFunction)

---
+++ GameProject20223/Classes/Button/RestartButton.cs
@@ -0,0 +1,24 @@
+﻿using GameProject20223.Classes.GameStates;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Button
+{
+    internal class RestartButton : cButton
+    {
+        public RestartButton(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, int X, int Y) : base(game, graphicsDevice, content, X, Y)
+        {
+            _texture = content.Load<Texture2D>("restart");
+        }
+
+        internal override void DoBtnFunction()
+        {
+            _game.ChangeState(new Playing(_game, _graphicsDevice, _content));
+        }
+    }
+}
