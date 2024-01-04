e04dd0920a10235e95a43b721f91ac340c998f76
hamza <hamza2140@outlook.com>
2024-01-04 16:53:25 +0100

    Menubutton klasse maken en inladen

---
+++ GameProject20223/Classes/Button/MenuButton.cs
@@ -0,0 +1,21 @@
+﻿using GameProject20223.Classes.GameStates;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Button
+{
+    internal class MenuButton : cButton
+    {
+        public MenuButton(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, int X, int Y) : base(game, graphicsDevice, content, X, Y)
+        {
+            _texture = content.Load<Texture2D>("menu");
+        }
+
+        
+    }
+}
