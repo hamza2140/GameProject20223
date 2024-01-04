522dadaedb2372135e881751f42557a7457e206c
hamza <hamza2140@outlook.com>
2024-01-04 17:18:36 +0100

    MainMenu klasse maken en beginnen met implementeren inheritance van GameState

--- GameProject20223/Classes/GameStates/MainMenu.cs
+++ GameProject20223/Classes/GameStates/MainMenu.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using GameProject20223.Classes.Button;
+using GameProject20223.Classes.Levelsdesign;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameProject20223.Classes.GameStates
 {
-    internal class MainMenu
+    internal class MainMenu : GameState
     {
+        new Game1 _game;
 
+        private Texture2D _backgroundTexture;
+        private Background background;
+
+        public MainMenu(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            // Sleep zodat er niet tweemaal op een knop word gedrukt na game over
+            Thread.Sleep(200);
+
+            // Game inladen
+            _game = game;
+
+            // Textures inladen
+            _backgroundTexture = _content.Load<Texture2D>("mainscreen");
+
+            // Initialiseren
+            background = new Background(_backgroundTexture);
+
+            // Knoppen toevoegen
+            buttons.Add(new StartButton(game, graphicsDevice, content, 610, 400));
+            buttons.Add(new CloseButton(game, graphicsDevice, content, 610, 480));
+        }
     }
 }
