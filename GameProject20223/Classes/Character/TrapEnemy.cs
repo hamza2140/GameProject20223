f65a687f1d0dd140c15394d79dc08e3e96c29994
hamza <hamza2140@outlook.com>
2024-01-04 15:04:31 +0100

    TrapEnemy klasse maken en constructor initialiseren

---
+++ GameProject20223/Classes/Character/TrapEnemy.cs
@@ -0,0 +1,26 @@
+﻿using GameProject20223.Animations;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject20223.Classes.Character
+{
+    class TrapEnemy : Enemy
+    {
+        Animation staticAnimation;
+
+        public TrapEnemy(Texture2D texture, int startPos, int endPos, int height) : base(texture, startPos, endPos, height)
+        {
+            base.width = 32;
+            base.height = 32;
+            base.scale = 1;
+
+            MakeAnimations();
+
+            animationManager.CurrentAnimation = staticAnimation;
+        }
+    }
+}
